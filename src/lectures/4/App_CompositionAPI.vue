5ec861ef152ce8a52d4805a8c7d9da14bb442bad
kimyezzang97 <kimyezzang97@gmail.com>
2023-05-22 01:56:01 +0900

    컴포지션 API lint 적용

--- src/lectures/4/App_CompositionAPI.vue
+++ src/lectures/4/App_CompositionAPI.vue
@@ -1,54 +1,54 @@
 <template>
-  <div>
-    <h2>반응형</h2>
-    <p>{{ reactiveMessage }}</p>
-    <button v-on:click="addReactiveMesssage">Add message</button>
-    <h2>일반</h2>
-    <p>{{ normalMessage }}</p>
-    <button v-on:click="addNormalMesssage">Add message</button>
-  </div>
+	<div>
+		<h2>반응형</h2>
+		<p>{{ reactiveMessage }}</p>
+		<button v-on:click="addReactiveMesssage">Add message</button>
+		<h2>일반</h2>
+		<p>{{ normalMessage }}</p>
+		<button v-on:click="addNormalMesssage">Add message</button>
+	</div>
 </template>
 
 <script>
-import { isRef, onBeforeMount, onMounted, onUpdated, ref } from 'vue'
+import { isRef, onBeforeMount, onMounted, onUpdated, ref } from 'vue';
 
 export default {
-  setup() {
-    console.log('setup()');
-    // 반응형 상태 선언
-    const reactiveMessage = ref('Reactive Message')
-    // 일반 변수 선언
-    let normalMessage = 'Normal Message'
+	setup() {
+		console.log('setup()');
+		// 반응형 상태 선언
+		const reactiveMessage = ref('Reactive Message');
+		// 일반 변수 선언
+		let normalMessage = 'Normal Message';
 
-    //console.log('isRef(reactiveMessage): ', isRef(reactiveMessage)) // true
-    //console.log('isRef(normalMessage): ', isRef(normalMessage)) // false
+		console.log('isRef(reactiveMessage): ', isRef(reactiveMessage)); // true
+		//console.log('isRef(normalMessage): ', isRef(normalMessage)) // false
 
-    const addReactiveMesssage = () => {
-      reactiveMessage.value = reactiveMessage.value + '!'
-    }
-    const addNormalMesssage = () => {
-      normalMessage = normalMessage + '!'
-    }
+		const addReactiveMesssage = () => {
+			reactiveMessage.value = reactiveMessage.value + '!';
+		};
+		const addNormalMesssage = () => {
+			normalMessage = normalMessage + '!';
+		};
 
-    onBeforeMount(() => {
-      console.log('onBeforeMount');
-    })
-    onMounted(() =>{
-      console.log('onMounted');
-    })
+		onBeforeMount(() => {
+			console.log('onBeforeMount');
+		});
+		onMounted(() => {
+			console.log('onMounted');
+		});
 
-    onUpdated(() => {
-      console.log('update component')
-    })
+		onUpdated(() => {
+			console.log('update component');
+		});
 
-    return {
-      reactiveMessage,
-      normalMessage,
-      addReactiveMesssage,
-      addNormalMesssage
-    }
-  }
-}
+		return {
+			reactiveMessage,
+			normalMessage,
+			addReactiveMesssage,
+			addNormalMesssage,
+		};
+	},
+};
 </script>
 
 <style lang="scss" scoped></style>
