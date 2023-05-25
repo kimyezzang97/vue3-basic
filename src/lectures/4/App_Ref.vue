67c2331860a4e416b52e72c6b7a1ebc67a9df29c
kimyezzang97 <kimyezzang97@gmail.com>
2023-05-25 20:12:41 +0900

    Ref

---
+++ src/lectures/4/App_Ref.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<p>{{ message }}</p>
+		<button @click="addMessage">add Click</button>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	// primitive 변수 적용
+	setup() {
+		let message = ref('Hello Vue');
+		const addMessage = () => {
+			message.value += '!';
+		};
+		return { message, addMessage };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
