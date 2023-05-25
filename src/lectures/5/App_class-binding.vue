64e1524318b3f9d51169ed25f4073db93fc2a8bf
kimyezzang97 <kimyezzang97@gmail.com>
2023-05-25 23:40:38 +0900

    Create App_class-binding.vue

---
+++ src/lectures/5/App_class-binding.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<div :class="{ active: isActive }">텍스트입니다.</div>
+		<button @click="toggle">toggle</button>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const isActive = ref(true);
+
+		const toggle = () => {
+			isActive.value = !isActive.value;
+		};
+		return { isActive, toggle };
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+.active {
+	font-weight: 900;
+}
+</style>
