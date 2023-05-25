f7188947b03412e8be39bc5d8dd04b6abf6f63d3
kimyezzang97 <kimyezzang97@gmail.com>
2023-05-25 23:40:41 +0900

    Create App_style_bindging.vue

---
+++ src/lectures/5/App_style_bindging.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<div :style="styleObject">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic eligendi,
+			doloremque ipsam maxime rem nisi veniam voluptates, soluta fugiat ea saepe
+			praesentium, cupiditate consequuntur numquam enim qui quia nesciunt
+			minima?
+		</div>
+		<button @click="fontSize--">-</button>
+		<button @click="fontSize++">+</button>
+	</div>
+</template>
+
+<script>
+import { computed } from 'vue';
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const fontSize = ref(13);
+		const styleObject = computed(() => {
+			return {
+				color: 'red',
+				fontSize: fontSize.value + 'px',
+			};
+		});
+		return { fontSize, styleObject };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
