f61505cff108225bb42645e63c2a0800113f2ed6
kimyezzang97 <kimyezzang97@gmail.com>
2023-05-27 22:26:44 +0900

    이벤트 수식어

---
+++ src/lectures/event/App_mouse.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+		<div id="modifiers">
+			<div @click="clickDiv">
+				DIV 영역
+				<p @click="clickP">
+					P 영역
+					<span @click.stop="clickSpan">SPAN 영역</span>
+				</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	setup() {
+		const clickDiv = () => {
+			console.log('clickDiv');
+			location.href = 'https://naver.com';
+		};
+		const clickP = () => {
+			console.log('clickP');
+		};
+		const clickSpan = () => {
+			console.log('clickSpan');
+			alert('좋아요');
+		};
+		return { clickDiv, clickP, clickSpan };
+	},
+};
+</script>
+
+<style lang="scss" scoped>
+#modifiers div,
+#modifiers p,
+#modifiers span {
+	padding: 40px;
+}
+#modifiers div {
+	background-color: #ccc;
+}
+#modifiers p {
+	background-color: #999;
+}
+#modifiers span {
+	background-color: #666;
+	display: block;
+}
+</style>
