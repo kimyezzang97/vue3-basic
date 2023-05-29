fc1190b2d75d97c28f495e60ff4fd71a52254a50
kimyezzang97 <kimyezzang97@gmail.com>
2023-05-29 22:29:26 +0900

    Create App_keyboard.vue

---
+++ src/lectures/event/App_keyboard.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<input type="text" @keyup.enter="appTodo" value="" />
+		<ul>
+			<li v-for="(todo, index) in todos" :key="index">
+				{{ todo }}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+import { reactive, ref } from 'vue';
+
+export default {
+	setup() {
+		const todos = reactive([]);
+
+		const appTodo = e => {
+			todos.push(event.target.value);
+			event.target.value = '';
+			event.target.focus();
+		};
+		return { todos, appTodo };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
