109abc690344c12b6a32bb7e6298ca73c315424c
kimyezzang97 <kimyezzang97@gmail.com>
2023-05-30 22:42:24 +0900

    컴포넌트 기초

---
+++ src/components/TheVIew.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<main>
+			<div class="container">
+				<div class="container text-center py-4">
+					<div class="row g-3">
+						<div class="col col-4">
+							<AppCard />
+						</div>
+						<div class="col col-4"><AppCard /></div>
+						<div class="col col-4"><AppCard /></div>
+						<div class="col col-4"><AppCard /></div>
+						<div class="col col-4"><AppCard /></div>
+						<div class="col col-4"><AppCard /></div>
+					</div>
+				</div>
+			</div>
+		</main>
+	</div>
+</template>
+
+<script>
+import AppCard from './AppCard.vue';
+
+export default {
+	setup() {
+		return {};
+	},
+	components: { AppCard },
+};
+</script>
+
+<style lang="scss" scoped></style>
